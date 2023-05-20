21fd67a68cec0ae22c4b85462747e99983ecc8b4
erdogansad <60552846+erdogansad@users.noreply.github.com>
2023-05-20 14:31:38 +0300

    quote fix

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, NavLink, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AppFunctional from './components/AppFunctional'
 import './styles/reset.css'
 import './styles/styles.css'
@@ -10,7 +10,7 @@ const root = createRoot(container)
 
 root.render(
   <BrowserRouter>
-    <h1>GRID'e hoşgeldiniz</h1>
+    <h1>GRID{"'"}e hoşgeldiniz</h1>
     <Routes>
       <Route path="/" element={<AppFunctional className="functional" />} />
     </Routes>
